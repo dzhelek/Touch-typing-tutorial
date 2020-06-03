74647a5208406ac4658d0ca1cefec146c7048770
A.Stoev <antoni.1998@abv.bg>
2020-06-03 13:29:07 +0300

    Add keyboard to tutorial screen

--- Touch-typing-tutorial/basics.py
+++ Touch-typing-tutorial/basics.py
@@ -30,6 +30,19 @@ LEFT_HAND = '''
   |    |
 '''
 
+KEYBOARD = '''
+,---,---,---,---,---,---,---,---,---,---,---,---,---,-------,
+|1/2| 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 0 | + | ' | <-    |
+|---'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-----|
+| ->| | Q | W | E | R | T | Y | U | I | O | P | ] | ^ |     |
+|-----',--',--',--',--',--',--',--',--',--',--',--',--'|    |
+| Caps | A | S | D | F | G | H | J | K | L | \ | [ | * |    |
+|----,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'---'----|
+|    | < | Z | X | C | V | B | N | M | , | . | - |          |
+|----'-,-',--'--,'---'---'---'---'---'---'-,-'---',--,------|
+| ctrl |  | alt |                          |altgr |  | ctrl |
+'------'  '-----'--------------------------'------'  '------'
+'''
 
 mapper = {'a': ((1, 1), 0)}
 
@@ -55,19 +68,19 @@ def exit_on_q(key):
 def welcome():
     clear_screen()
     print(colored('----- Tutorial -----'.center(os.get_terminal_size().columns), 'blue'))
-    print('\n\n\n')
+    print('\n\n')
 
 
-def print_hands(left_hand=LEFT_HAND, right_hand=RIGHT_HAND):
-    strings=[left_hand, right_hand]
-    print(*['                                        '.join(x).center(os.get_terminal_size().columns) for x in zip(*[[x.ljust(len(max(s.split('\n'), key=len))) for x in s.split('\n')] for s in strings])], sep='\n')
+def print_hands_with_console(left_hand=LEFT_HAND, keyboard=KEYBOARD, right_hand=RIGHT_HAND):
+    strings=[left_hand, keyboard, right_hand]
+    print(*['    '.join(x).center(os.get_terminal_size().columns) for x in zip(*[[x.ljust(len(max(s.split('\n'), key=len))) for x in s.split('\n')] for s in strings])], sep='\n')
 
 
 def print_screen(text):
     welcome()
     print(text.center(os.get_terminal_size().columns))
-    print('\n\n\n')
-    print_hands()
+    print('\n\n')
+    print_hands_with_console()
 
 if __name__ == '__main__':
     print_screen(text)
